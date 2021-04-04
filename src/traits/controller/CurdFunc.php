cce833fc3c00496a3e0358bead7f3010c526ae10
tjztjspz <1079798840@qq.com>
2021-04-04 17:46:34 +0800

    bug

--- src/traits/controller/CurdFunc.php
+++ src/traits/controller/CurdFunc.php
@@ -136,7 +136,8 @@ trait CurdFunc
             $info=null;
         }
 
-        if($fields->saveStepInfo&&$fields->saveStepInfo->authCheck($info,$baseModel,$fields)===false){
+
+        if($fields->saveStepInfo&&$fields->saveStepInfo->authCheck($data,$baseModel,$fields)===false){
             return $this->error('您不能进行此操作');
         }
 
