4624a8f12c90f94fce5aed4d80b977677e1559e7
tjztj <1079798840@qq.com>
2021-04-10 13:58:32 +0800

    Update edit.js

--- tp-script-vue-curd-static/field/region/edit.js
+++ tp-script-vue-curd-static/field/region/edit.js
@@ -73,7 +73,7 @@ define([],function(){
                                 :placeholder="field.placeholder||'请选择村社'"
                                 show-search
                                 :disabled="field.readOnly"
-                                :change-on-select="canCheckParent"
+                                :change-on-select="field.canCheckParent"
                                 @change="onRegionChange"
                             />
                         </div>
