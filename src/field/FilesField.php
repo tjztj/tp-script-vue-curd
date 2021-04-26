878ffeab9d79fd6014fa3d76ccf24f44628b5ae8
tjztjspz <1079798840@qq.com>
2021-04-26 15:27:16 +0800

    增加文件上传字段

---
+++ src/field/FilesField.php
@@ -0,0 +1,90 @@
+<?php
+
+
+namespace tpScriptVueCurd\field;
+use tpScriptVueCurd\ExcelFieldTpl;
+use tpScriptVueCurd\ModelField;
+use tpScriptVueCurd\tool\field_tpl\Edit;
+use tpScriptVueCurd\tool\field_tpl\FieldTpl;
+use tpScriptVueCurd\tool\field_tpl\Index;
+use tpScriptVueCurd\tool\field_tpl\Show;
+
+
+/**
+ * 文件
+ * Class FilesField
+ * @author tj 1079798840@qq.com
+ * @package tpScriptVueCurd
+ */
+class FilesField extends ModelField
+{
+
+    protected string $url='';//默认值是 tpScriptVueCurdUploadDefaultUrl
+    protected bool $canExcelImport=false;//不能使用excel导入数据
+
+
+    /**最小值
+     * @param string|null $url
+     * @return $this|string
+     */
+    public function url(string $url = null)
+    {
+        if(is_null($url)){
+            return $this->url?:tpScriptVueCurdUploadDefaultUrl();
+        }
+        $this->url=$url;
+        return $this;
+    }
+
+
+
+    /**
+     * 设置保存的值
+     * @param array $data  数据值集合
+     * @return $this
+     */
+    public function setSaveVal(array $data): self
+    {
+        if(isset($data[$this->name()])){
+            $this->save=$data[$this->name()];
+        }
+        $this->defaultCheckRequired($this->save,'请上传附件');
+        return $this;
+    }
+
+
+    /**
+     * 显示时对数据处理
+     * @param array $dataBaseData
+     */
+    public function doShowData(array &$dataBaseData): void
+    {
+        parent::doShowData($dataBaseData);
+        if(isset($dataBaseData[$this->name()])){
+            $dataBaseData[$this->name()]=trim($dataBaseData[$this->name()]);
+            $dataBaseData[$this->name().'Arr']=$dataBaseData[$this->name()]?explode('|',$dataBaseData[$this->name()]):[];
+        }
+    }
+
+
+    /**
+     * 模板导入备注
+     * @param ExcelFieldTpl $excelFieldTpl
+     * @return void
+     */
+    public function excelTplExplain(ExcelFieldTpl $excelFieldTpl):void{
+        $excelFieldTpl->explain="此列不可导入数据";
+    }
+
+
+    public static function componentUrl(): FieldTpl
+    {
+        $type=class_basename(static::class);
+        return new FieldTpl($type,
+            new Index($type,'/tp-script-vue-curd-static.php?field/files/index.js'),
+            new Show($type,'/tp-script-vue-curd-static.php?field/files/show.js'),
+            new Edit($type,'/tp-script-vue-curd-static.php?field/files/edit.js')
+        );
+    }
+
+}
---
+++ tp-script-vue-curd-static/field/files/edit.js
@@ -0,0 +1,80 @@
+define([],function(){
+    return {
+        props:['field','value','validateStatus'],
+        setup(props,ctx){
+            let fileList=Vue.ref([]),id='upload-class-'+window.guid();
+            if(props.value){
+                let imgList=props.value.split('|'),fid=0;
+                fileList.value=imgList.map(function(v){
+                    fid--;
+                    return {
+                        uid:fid,
+                        name:v.substring(v.lastIndexOf("/")+1,v.length),
+                        status: 'done',
+                        url:v,
+                    };
+                })
+            }
+            return {
+                fileList,
+                id
+            }
+        },
+        methods:{
+            handleRemove(){
+                return file => {
+                    if(file.url){
+                        this.$emit('update:value',this.value.split('|').filter(url=>url&&url!==file.url).join('|'));
+                    }
+                }
+            },
+            handleChange(data,field) {
+                let urls=[];
+                this.fileList =data.fileList.map(function(file){
+                    if(file.status==='done'){
+                        if(file.response){
+                            if(file.response.code==0){
+                                antd.message.error('文件[ '+file.name+' ]：'+file.response.msg,6);
+                            }else{
+                                file.url=file.response.data.url
+                            }
+                        }
+                    }
+                    return file;
+                }).filter(function(file){
+                    if(file.status==='done'){
+                        if(urls.indexOf(file.url)!==-1||!file.url){
+                            return false;
+                        }
+                        urls.push(file.url)
+                    }
+                    return true;
+                });
+                let value=urls.join('|');
+                this.$emit('update:value',value);
+                this.$emit('update:validateStatus',value?'success':'error');
+
+            },
+        },
+        template:`<div class="field-box" :class="[id]">
+                    <div class="l">
+                        <a-upload
+                            multiple
+                            :action="field.url"
+                            list-type="text"
+                            :file-list="fileList"
+                            :remove="handleRemove"
+                            :disabled="field.readOnly"
+                            @change="handleChange"
+                        >
+                        <a-button size="small">
+                        <span role="img" aria-label="upload" class="anticon anticon-upload"><svg class="" data-icon="upload" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896" focusable="false"><path d="M400 317.7h73.9V656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V317.7H624c6.7 0 10.4-7.7 6.3-12.9L518.3 163a8 8 0 00-12.6 0l-112 141.7c-4.1 5.3-.4 13 6.3 13zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"></path></svg></span>
+                        上传</a-button>
+                        </a-upload>
+                    </div>
+                    <div class="r">
+                        <span v-if="field.ext" class="ext-span">{{ field.ext }}</span>
+                    </div>
+        </div>`,
+    }
+});
