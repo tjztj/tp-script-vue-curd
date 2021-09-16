d4fea2e91aa3fa8e092565b36e7a6f3837e0d1e6
tjztjspz <1079798840@qq.com>
2021-09-16 14:54:23 +0800

    test

--- copy/install.php
+++ copy/install.php
@@ -2,12 +2,8 @@
 
 namespace tpScriptVueCurdCopy;
 
-use Composer\Script\Event;
 class install
 {
 
-    public static function copyAboutFiles(Event $event):void{
-        var_dump(dirname(__DIR__) . DIRECTORY_SEPARATOR) ;
 
-    }
 }
