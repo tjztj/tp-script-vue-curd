dd13bbb6a4109c9c5ff75ecec1da98ca1a5dcba3
tjztjspz <1079798840@qq.com>
2021-10-16 18:02:26 +0800

    地区字段优化

--- src/filter/RegionFilter.php
+++ src/filter/RegionFilter.php
@@ -21,48 +21,11 @@ class RegionFilter extends ModelFilter
     }
     public function generateWhere(Query $query,$value):void{
         if($value){
-            $ids=[$value];
-            $getChildIds=function ($tree,$isChecked)use($value,&$ids,&$getChildIds){
-                foreach ($tree as $v){
-                    if($isChecked){
-                        $ids[]=$v['value'];
-                        if(!empty($v['children'])){
-                            $getChildIds($v['children'],true);
-                        }
-                    }else{
-                        if($v['value']===$value){
-                            empty($v['children'])||$getChildIds($v['children'],true);
-                            break;
-                        }else{
-                            $getChildIds($v['children'],false);
-                        }
-                    }
-                }
-            };
-            $getChildIds($this->field->getRegionTree(),false);
-
-
-            if(count($ids)>100){
-                $query->where(function (Query $q)use($value){
-                    $q->whereRaw($this->field->name().' IN '.\app\admin\model\SystemRegion::whereFindInSet('pids',$value)
-                            ->field('id')->buildSql())
-                        ->whereOr($this->field->name(),$value);
-                });
-            }else{
-                $name=$this->field->name();
-                if($this->field->pField()){
-                    $name.='|'.$this->field->pField();
-                }else if($this->field->cField()){
-                    $name.='|'.$this->field->cField();
-                }else{
-                    $query->where($name,'in',$ids);
-                    return;
-                }
-                $query->where(function (Query $q)use($ids,$name,$value){
-                    $q->whereOr($name,$value)->whereOr($this->field->name(),'in',$ids);
-                });
-            }
-
+            $query->where(function (Query $q)use($value){
+                $q->whereRaw($this->field->name().' IN '.\app\admin\model\SystemRegion::whereFindInSet('pids',$value)
+                        ->field('id')->buildSql())
+                    ->whereOr($this->field->name(),$value);
+            });
         }
     }
 
