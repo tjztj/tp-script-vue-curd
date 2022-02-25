03e87c66b2edd4c470272f2e421fae3a6757da46
tjztjspz <1079798840@qq.com>
2022-02-25 14:42:46 +0800

    流程图美化

--- tp-script-vue-curd-static/actions/step.js
+++ tp-script-vue-curd-static/actions/step.js
@@ -4,12 +4,70 @@ define(['vueAdmin','g6'], function (va,G6) {
 
 
     actions.bpmn=function (){
-        let graph=null;
+        G6.registerEdge(
+            'polyline-running',
+            {
+                afterDraw(cfg, group) {
+                    // get the first shape in the group, it is the edge's path here=
+                    const shape = group.get('children')[0];
+                    // the start position of the edge's path
+                    const startPoint = shape.getPoint(0);
+
+                    // add red circle shape
+                    const circle = group.addShape('circle', {
+                        attrs: {
+                            x: startPoint.x,
+                            y: startPoint.y,
+                            fill: '#722ed1',
+                            r: 1,
+                        },
+                        name: 'circle-shape',
+                    });
+
+                    // animation for the red circle
+                    circle.animate(
+                        (ratio) => {
+                            // the operations in each frame. Ratio ranges from 0 to 1 indicating the prograss of the animation. Returns the modified configurations
+                            // get the position on the edge according to the ratio
+                            const tmpPoint = shape.getPoint(ratio);
+                            // returns the modified configurations here, x and y here
+                            return {
+                                x: tmpPoint.x,
+                                y: tmpPoint.y,
+                            };
+                        },
+                        {
+                            repeat: true, // Whether executes the animation repeatly
+                            duration: 5000, // the duration for executing once
+                        },
+                    );
+                },
+            },
+            'polyline', // extend the built-in edge 'cubic'
+        );
+
+
+
+
+        let graph=null,changan=null,changanIng=false;
         return {
             data(){
                 return {
-
-              }
+                    svg1:`<svg t="1645753872304" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4793"><path d="M64.48671 349.378226l895.354038 0 0 325.583287-895.354038 0 0-325.583287Z" p-id="4794"></path><path d="M349.372086 64.493873l325.583287 0 0 895.354038-325.583287 0 0-895.354038Z" p-id="4795"></path></svg>`,
+                    svg2:`<svg t="1645753933844" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2552"><path d="M64.322981 349.208357l895.353015 0 0 325.583287-895.353015 0 0-325.583287Z" p-id="2553"></path></svg>`,
+                    nodeFontSize:4,
+                    edgeFontSize:4,
+                    size:100,
+                    isInit:false,
+                    defWidth:document.documentElement.clientWidth-54,
+                    defHeight:document.documentElement.clientHeight-62,
+                    showAnimation:true,
+                }
+            },
+            watch:{
+                showAnimation(){
+                    graph.render(); // 渲染图
+                }
             },
             mounted() {
                 this.pageIsInit();
@@ -23,11 +81,15 @@ define(['vueAdmin','g6'], function (va,G6) {
             },
 
             methods:{
+                getSize(){
+                    return [parseInt(this.defWidth*(this.size/100)),parseInt(this.defHeight*(this.size/100))];
+                },
                 init(){
+                    const size=this.getSize();
                     graph = new G6.Graph({
                         container: 'mountNode', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
-                        width: document.documentElement.clientWidth-54, // Number，必须，图的宽度
-                        height: document.documentElement.clientHeight-62, // Number，必须，图的高度
+                        width: size[0], // Number，必须，图的宽度
+                        height: size[1], // Number，必须，图的高度
                         fitView: true,
                         animate:true,
                         modes: {
@@ -36,11 +98,12 @@ define(['vueAdmin','g6'], function (va,G6) {
                         layout: {
                             type: 'dagre',
                             rankdir: 'LR',
-                            align: 'UL',
+                            // align: 'UL',
                             controlPoints: true,
-                            nodesepFunc: () => 1,
+                            nodesepFunc: () => 8,
                             ranksepFunc: () => 1,
                         },
+
                         defaultNode: {
                             size: [30, 20],
                             type: 'rect',
@@ -49,14 +112,10 @@ define(['vueAdmin','g6'], function (va,G6) {
                                 stroke: '#5B8FF9',
                                 fill: '#C6E5FF',
                             },
-                            labelCfg:{
-                                style:{
-                                    fontSize:4,
-                                }
-                            },
                         },
                         defaultEdge: {
-                            type: 'polyline',
+                            type: 'polyline-running',
+                            // type: 'polyline',
                             size: 1,
                             color: '#e2e2e2',
                             style: {
@@ -66,24 +125,86 @@ define(['vueAdmin','g6'], function (va,G6) {
                                 },
                                 radius: 20,
                             },
-                            labelCfg:{
-                                style:{
-                                    fontSize:4,
-                                }
-                            },
                         },
                     });
 
 
 
-
+                    graph.node((node) => {
+                        return {
+                            labelCfg:{
+                                style:{
+                                    fontSize:this.nodeFontSize,
+                                }
+                            },
+                        };
+                    });
+                    graph.edge((edge) => {
+                        return {
+                            type:this.showAnimation?'polyline-running':'polyline',
+                            labelCfg:{
+                                style:{
+                                    fontSize:this.edgeFontSize,
+                                }
+                            },
+                        };
+                    });
                     graph.data(vueData.data); // 读取 Step 2 中的数据源到图上
                     graph.render(); // 渲染图
+                    this.isInit=true;
                 },
+
+                addNodeF(){
+                    this.nodeFontSize++;
+                    graph.render(); // 渲染图
+                },
+                subNodeF(){
+                    if(this.nodeFontSize<=1)return;
+                    this.nodeFontSize--;
+                    graph.render(); // 渲染图
+                },
+                addEdgeF(){
+                    this.edgeFontSize++;
+                    graph.render(); // 渲染图
+                },
+                subEdgeF(){
+                    if(this.edgeFontSize<=1)return;
+                    this.edgeFontSize--;
+                    graph.render(); // 渲染图
+                },
+                addSize(){
+                    console.log(graph.getZoom());
+                    this.size++;
+                    graph.changeSize(...this.getSize());
+                    graph.render();
+                },
+                subSize(){
+                    if(this.size<=1)return;
+                    this.size--;
+                    graph.changeSize(...this.getSize());
+                    graph.render();
+                },
+                startChangAn(func,step){
+                    if(changan){
+                        window.clearInterval(changan);
+                    }
+                    changanIng=true;
+                    setTimeout(()=>{
+                        if(changanIng){
+                            changan=setInterval(func,40)
+                        }
+                    },step||120)
+                },
+                endChangAn(){
+                    if(changan){
+                        window.clearInterval(changan);
+                    }
+                    changan=null;
+                    changanIng=false;
+                }
             },
         }
     };
-
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     let return_actions = {};
--- tpl/step/bpmn.vue
+++ tpl/step/bpmn.vue
@@ -1,1 +1,98 @@
+<style>
+body, html{
+    padding: 0;
+}
+#app{
+    background-color: transparent;
+}
+#tool-box{
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+}
+.tool-item{
+    display: flex;
+    background-color: #fff;
+    padding: 12px;
+    border-radius: 2px;
+    box-shadow: 0px 1px 2px -2px rgba(0, 0, 0, 0.16),0px 3px 6px 0 rgba(0, 0, 0, 0.12),0px 5px 12px 4px rgba(0, 0, 0, 0.09);
+    align-items: center;
+}
+.tool-item{
+    margin: 12px;
+}
+.tool-item-btns{
+    display: flex;
+    align-items: center;
+}
+.tool-item-btns{
+    margin-left: 4px;
+    border: 1px solid #bfbfbf;
+    padding: 4px 8px;
+    border-radius: 4px;
+}
+.show-val{
+    padding: 0 8px;
+    font-weight: bold;
+    color: #222;
+}
+svg.icon{
+    width: 1em;
+    height: 1em;
+    vertical-align: text-top;
+    fill: currentColor;
+    overflow: hidden;
+}
+.tool-item-btns svg.icon{
+    cursor: pointer;
+    transition: all .3s;
+    color: #8c8c8c;
+}
+.tool-item-btns svg.icon:hover{
+    color: #597ef7;
+}
+.item-enlarge,.item-narrow{
+    line-height: 1em;
+    height: 1em;
+}
+#mountNode>canvas{
+    margin: 24px;
+    background-color: #fff;
+}
+</style>
+
+<div id="tool-box" v-show="isInit">
+<div class="item-label-font tool-item">
+    <div class="tool-item-text">节点文字</div>
+    <div class="tool-item-btns">
+        <div class="item-enlarge" v-html="svg1" @click="addNodeF" @mousedown="startChangAn(addNodeF,200)" @mouseup="endChangAn()" @mouseout="endChangAn()"></div>
+        <div class="show-val">{{nodeFontSize}}</div>
+        <div class="item-narrow" v-html="svg2" @click="subNodeF" @mousedown="startChangAn(subNodeF,200)" @mouseup="endChangAn()" @mouseout="endChangAn()"></div>
+    </div>
+</div>
+<div class="edge-label-font tool-item">
+    <div class="tool-item-text">条件文字</div>
+    <div class="tool-item-btns">
+        <div class="item-enlarge" v-html="svg1" @click="addEdgeF" @mousedown="startChangAn(addEdgeF,200)" @mouseup="endChangAn()" @mouseout="endChangAn()"></div>
+        <div class="show-val">{{edgeFontSize}}</div>
+        <div class="item-narrow" v-html="svg2" @click="subEdgeF" @mousedown="startChangAn(subEdgeF,200)" @mouseup="endChangAn()" @mouseout="endChangAn()"></div>
+    </div>
+</div>
+<div class="window-label-font tool-item">
+    <div class="tool-item-text">整体</div>
+    <div class="tool-item-btns">
+        <div class="item-enlarge" v-html="svg1" @click="addSize" @mousedown="startChangAn(addSize,40)" @mouseup="endChangAn()" @mouseout="endChangAn()"></div>
+        <div class="show-val">{{size}}%</div>
+        <div class="item-narrow" v-html="svg2" @click="subSize" @mousedown="startChangAn(subSize,40)" @mouseup="endChangAn()" @mouseout="endChangAn()"></div>
+    </div>
+</div>
+<div class="window-label-font tool-item">
+    <div class="tool-item-text">动画</div>
+    <div class="tool-item-btns">
+        <a-switch size="small" v-model:checked="showAnimation"></a-switch>
+    </div>
+</div>
+</div>
 <div id="mountNode"></div>
