32e4dbe4f1c1c287b25d6ba75230a53cc7463d3e
tjztjspz <1079798840@qq.com>
2022-08-31 17:55:40 +0800

    优化

--- tp-script-vue-curd-static/row_other_btn/show_inputs.vue
+++ tp-script-vue-curd-static/row_other_btn/show_inputs.vue
@@ -52,7 +52,7 @@
     </style>
 </head>
 <body style="background-color: #fff">
-<div id="app-loading" style="display: none">
+<div id="app-loading">
     <svg viewBox="25 25 50 50">
         <circle cx="50" cy="50" r="20"></circle>
     </svg>
