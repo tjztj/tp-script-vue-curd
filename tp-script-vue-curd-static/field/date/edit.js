1982efbd8f9fba4b6d107ecc2276d4a7c55b9ed4
tjztjspz <1079798840@qq.com>
2022-10-29 15:18:24 +0800

    时间选择优化

--- tp-script-vue-curd-static/field/date/edit.js
+++ tp-script-vue-curd-static/field/date/edit.js
@@ -1,6 +1,51 @@
 define([], function () {
+    const styleId='date-field-edit-dropdown-stype';
+    const style = `
+<style id="${styleId}">
+.date-field-edit-dropdown .ant-calendar-time-picker-panel{
+    left: 100%;
+    padding: 74px 0 33px 0;
+    top: -75px;
+    background-color: #fff;
+    box-shadow: 6px 0px 8px rgb(0 0 0 / 15%);
+    width: 160px;
+}
+.date-field-edit-dropdown .ant-calendar-time-picker-inner{
+    border: 1px solid #f0f0f0;
+    border-right: 0;
+    width: 160px;
+}
+.date-field-edit-dropdown .ant-calendar-time-picker-panel:before{
+    display: block;
+    content: ' ';
+    position: absolute;
+    border-top: 1px solid #f0f0f0;
+    left: 0;
+    width: 160px;
+    top: 34px;
+}
+.date-field-edit-dropdown .ant-calendar-time-picker-btn{
+    display: none!important;
+}
+.date-field-edit-dropdown .ant-calendar-time-picker+.ant-calendar-body+.ant-calendar-footer-show-ok{
+  z-index: 1051;
+}
+.date-field-edit-dropdown .ant-calendar-time-picker+.ant-calendar-body+.ant-calendar-footer-show-ok .ant-calendar-ok-btn{
+margin-right: -160px;
+}
+</style>
+`
+
     return {
         props: ['field', 'value', 'validateStatus'],
+        setup(){
+            if (!document.getElementById(styleId)) {
+                document.querySelector('head').insertAdjacentHTML('beforeend', style);
+            }
+          return {
+              guid:window.guid(),
+          }
+        },
         computed: {
             dateDefaultValue: {
                 get() {
@@ -52,6 +97,48 @@ define([], function () {
             },
             moment(tm,f){
                 return moment(tm,f)
+            },
+            openChange(status){
+                if(!status){
+                   return;
+                }
+                this.$nextTick(()=>{
+                    if(!document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker')&&document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker-btn')){
+                        document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker-btn').click();
+                        this.doOpen();
+                    }
+                })
+            },
+            onChange(){
+                this.$nextTick(()=>{
+                    if(!document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker')&&document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker-btn')){
+                        document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker-btn').click();
+                        this.doOpen();
+                    }
+                })
+            },
+            doOpen(){
+                document.querySelectorAll('.dropdown'+this.guid+' .ant-calendar-ym-select a').forEach((btn)=>{
+                    if(btn.getAttribute('init')){
+                        return;
+                    }
+                    btn.setAttribute('init','1');
+                    btn.addEventListener('click',(e)=>{
+                        if(!e.target.classList.contains('ant-calendar-time-status')||e.target.classList.contains('ant-calendar-day-select')){
+                            return;
+                        }
+                        if(!document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker')){
+                            return;
+                        }
+                        document.querySelector('.dropdown'+this.guid+' .ant-calendar-time-picker-btn').click();
+                        this.$nextTick(()=>{
+                            e.target.click();
+                        })
+                    });
+                })
+
+
+
             }
         },
         template: `<div class="field-box">
@@ -64,6 +151,9 @@ define([], function () {
                             :disabled-date="disabledDate"
                             :show-time="field.showTime?{ defaultValue: moment('00:00:00', 'HH:mm:ss') }:false"
                             :allow-clear="!field.required"
+                            :dropdown-class-name="'date-field-edit-dropdown dropdown'+guid"
+                            @open-change="openChange"
+                            @change="onChange"
                             style="width: 100%;"
                         />
                     </div>
