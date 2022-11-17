7ff664ad1580b9021f665baebd53fada83293c92
tjztjspz <1079798840@qq.com>
2022-11-17 18:03:17 +0800

    新增字段 table select

--- tp-script-vue-curd-static/filter/select.js
+++ tp-script-vue-curd-static/filter/select.js
@@ -6,14 +6,32 @@ define([], function () {
     padding: 2px 7px;
     height: 26px;
 }
+.filter-select-url-dropdown .rc-virtual-list-holder{
+overflow-y: auto!important;
+}
+.filter-select-url-dropdown .rc-virtual-list-scrollbar-show{
+display: none;
+}
 </style>
 `;
     return {
+        components: {
+            VNodes: (_, { attrs }) => {
+                return attrs.vnodes;
+            },
+        },
         props: ['config'],
         setup(props,ctx){
             if (!document.getElementById(styleId)) {
                 document.querySelector('head').insertAdjacentHTML('beforeend', style);
             }
+            const supplyBatch=Vue.ref([]);
+            const selectPage=Vue.ref(1);
+            const selectSize=Vue.ref(30);
+            const selectTotal=Vue.ref(1);
+            const selectFilter=Vue.ref('');
+            const fetching=Vue.ref(false);
+
             const inputValue=Vue.ref(undefined);
             const onParentSearch = () => {
                 let val='';
@@ -32,7 +50,14 @@ define([], function () {
 
 
             return {
-                inputValue,onParentSearch
+                inputValue,
+                onParentSearch,
+                supplyBatch,
+                selectPage,
+                selectSize,
+                selectTotal,
+                selectFilter,
+                fetching
             }
         },
         computed:{
@@ -65,10 +90,90 @@ define([], function () {
             },
             filterOption(input, option) {
                 return option.props.title.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+            },
+            handlePopupScroll(e){
+                const { scrollHeight, scrollTop, clientHeight } = e.target
+                if (scrollHeight - scrollTop - clientHeight<32) {
+                    console.log('触底了')
+                    this.selectPage = this.selectPage + 1
+                    if(this.selectPage <= this.selectTotal){
+                        this.getTaskList()
+                    }
+                }
+            },
+            '$post'(...params){
+                return window.vueDefMethods.$post.call(this,...params,)
+            },
+            getTaskList () {
+                this.fetching=true;
+                this.$post(this.config.url,{
+                    page:this.selectPage,
+                    keywords:this.selectFilter,
+                    pageSize: this.selectSize,
+                }).then(res=>{
+                    this.selectTotal = Math.ceil(res.data.total/this.selectSize);
+                    res.data.data.forEach(v=>{
+                        this.supplyBatch.push({
+                            label:v[this.config.lableField],
+                            value:v[this.config.valueField]
+                        })
+                    })
+
+                    console.log(res);
+                    this.fetching = false;
+                }).catch(()=>{
+                    this.fetching = false;
+                });
+            },
+            doFilter(value){
+                this.selectFilter=value.trim();
+                this.selectPage=1;
+                this.selectTotal=1;
+                this.supplyBatch=[];
+                this.getTaskList();
+            },
+            onFocus(){
+                if(this.selectPage!==1||this.selectFilter!==''||this.supplyBatch.length>0){
+                    return;
+                }
+                this.doFilter('');
             }
         },
-        template: `<div>
-                    <div class="region-value-div">
+        template: `<div style="padding: 0 0.5em;margin: 0 0.1em;">
+                    <div v-if="config.url!==''">
+                         <a-select style="width: 236px" 
+                            v-model:value="inputValue"
+                            allow-clear
+                            show-search 
+                            size="small"
+                            placeholder="输入筛选信息"
+                            :default-active-first-option="false"
+                            :filter-option="false"
+                            :mode="config.multiple?'multiple':null"
+                            :options="supplyBatch"
+                            dropdown-class-name="filter-select-url-dropdown"
+                            @search="doFilter"
+                            @change="search"
+                            @popupScroll="handlePopupScroll"
+                            @focus="onFocus"
+                         >
+                          <template #dropdownRender="{ menuNode: menu }">
+                                <v-nodes :vnodes="menu" />
+                                <template v-if="fetching">
+                                    <template v-if="supplyBatch.length>0">
+                                        <div style="padding: 4px 16px;">
+                                            <a-spin size="small" />
+                                        </div>
+                                    </template>
+                                     <div v-else style="padding: 12px 16px"><a-spin size="small" /></div>
+                                </template>
+                          </template>
+                          <template  v-if="fetching" #notFoundContent>
+                              <span></span>
+                          </template>
+                        </a-select>
+                    </div>
+                    <div v-else class="region-value-div">
                         <a-select style="width: 236px" 
                               v-model:value="inputValue"
                               allow-clear
