91dc6cd8edbd0814e105def62adfd78881ed5ea0
tjztjspz <1079798840@qq.com>
2023-03-20 17:08:19 +0800

    优化

--- copy/tp-script-vue-curd-static.php
+++ copy/tp-script-vue-curd-static.php
@@ -1,52 +1,27 @@
 <?php
-const VUE_CURD_STATIC_PATH='..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'tj'.DIRECTORY_SEPARATOR.'tp-script-vue-curd'.DIRECTORY_SEPARATOR.'tp-script-vue-curd-static';
-
-$otherPre='/tp-script-vue-curd-static.php/';
-if( stripos($_SERVER['REQUEST_URI'],$otherPre)===0){
-    $param=mb_substr($_SERVER['REQUEST_URI'], strlen($otherPre), null, 'utf-8');
-}else{
-    $param=$_SERVER['QUERY_STRING']??'';
+$basePath = dirname(__DIR__) . '/vendor/tj/tp-script-vue-curd/tp-script-vue-curd-static';
+$dirs = array_filter(explode('/', str_replace('\\', '/', $_SERVER['QUERY_STRING'] ?? '')), 'trim');
+$lastDir = end($dirs);
+$endIndex1 = strpos($lastDir, '?');
+$endIndex2 = strpos($lastDir, '&');
+if ($endIndex1 !== false && $endIndex2 !== false) {
+    $endIndex = min($endIndex1, $endIndex2);
+} else {
+    $endIndex = $endIndex1 === false ? $endIndex2 : $endIndex1;
 }
-if(empty($param)){
-    header("HTTP/1.1 404 Not Found");
-    header("Status: 404 Not Found");
-    echo '未获取到文件名';
-    exit;
+if ($endIndex !== false) {
+    $dirs[count($dirs) - 1] = substr($lastDir, 0, $endIndex);
 }
-$param=str_replace(['\\','..','//'],['/','','/'],$param);
-
-$dirs=array_map(function($v){
-    return trim($v,'&');
-},array_filter(explode('/',$param)));
-$lastDir=end($dirs);
-$endIndex1=strpos($lastDir,'?');
-$endIndex2=strpos($lastDir,'&');
-if($endIndex1===false&&$endIndex2!==false){
-    $endIndex=$endIndex2;
-}else if($endIndex1!==false&&$endIndex2===false){
-    $endIndex=$endIndex1;
-}else if($endIndex1!==false&&$endIndex2!==false){
-    $endIndex=$endIndex1>$endIndex2?$endIndex2:$endIndex1;
-}else{
-    $endIndex=false;
+$path = realpath($basePath . '/' . implode('/', $dirs));
+if ($basePath === $path || strpos($path, $basePath . '/') !== 0 || !file_exists($path)) {
+    http_response_code(404);
+    exit();
 }
-$endIndex===false||$dirs[count($dirs)-1]=substr($lastDir,0,$endIndex);
-
-$path=VUE_CURD_STATIC_PATH.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR,$dirs);
-if(!is_file($path)){
-    header("HTTP/1.1 404 Not Found");
-    header("Status: 404 Not Found");
-//    echo '未找到:'.$path;
-    exit;
-}
-
-
-$ext=strtolower(pathinfo($path, PATHINFO_EXTENSION));
-if($ext==='css'){
+$ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+if ($ext === 'css') {
     header('Content-type:text/css');
-}else if($ext==='js'){
+} elseif ($ext === 'js') {
     header('Content-type:text/javascript');
 }
-//缓存
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
-echo file_get_contents($path);
+readfile($path);
