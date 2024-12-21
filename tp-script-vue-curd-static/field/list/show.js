dcaa88d472201437f4b6afe413b8a6a6cd26f2ad
tjztjspz <1079798840@qq.com>
2024-12-21 16:44:28 +0800

    优化

--- tp-script-vue-curd-static/field/list/show.js
+++ tp-script-vue-curd-static/field/list/show.js
@@ -1,11 +1,32 @@
 define([],function(){
     return {
         props:['info','field'],
+        computed:{
+            list(){
+                const list=this.info[this.field.name+'List'];
+                if(typeof list!=='undefined'){
+                    return list;
+                }
+                if(typeof this.info[this.field.name]==='undefined'){
+                    return [];
+                }
+
+                if(typeof this.info[this.field.name]==='object'){
+                    return Object.values(this.info[this.field.name])
+                }
+
+                try {
+                    return Object.values(JSON.parse(this.info[this.field.name]));
+                }catch (e){
+                    return [];
+                }
+            }
+        },
         methods:{
         },
         template:`<div>
                     <div class="list-field-box">
-                        <div class="list-field-item" v-for="(vo,key) in info[field.name+'List']">
+                        <div class="list-field-item" v-for="(vo,key) in list">
                             <div class="list-field-item-row" v-for="v in field.fields">
                                 <div class="list-field-item-row-l">{{v.title}}:</div>
                                 <div class="list-field-item-row-r"><curd-show-field :info="vo" :field="v"></curd-show-field></div>
